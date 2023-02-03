1dd8bda75f7669484a3a611ad09595440927c58f
Christophe Leam-Touch <e1449109@cmaisonneuve.qc.ca>
2023-02-03 09:29:57 -0500

    s2c2 - Ajout Pied de page

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
-<footer>
-    <h3>Pied de page</h3>
+<footer class="site__footer">
+    <section>Colonne 1</section>
+    <section>Colonne 2</section>
+    <section>Colonne 3</section>
 </footer>
 
 <!-- Prints scripts or data before the closing body tag on the front end -->
