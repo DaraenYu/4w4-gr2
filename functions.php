8cc9d8cbeb86f538b06b533a27d2e1c8d99d6a1a
Christophe Leam-Touch <e1449109@cmaisonneuve.qc.ca>
2023-02-03 10:41:07 -0500

    s2c2 Ajout menu + enqueue

--- functions.php
+++ functions.php
@@ -1,8 +1,27 @@
 <?php
-    wp_enqueue_style(
-        'style-principal'/* $handle */,
-        get_template_directory_uri() . '/style.css'/* $src */,
-        array() /* $deps */,
-        filemtime(get_template_directory() . '/style.css') /* $ver */,
-    );
+    // Enfiler la feuille de style
+    function ajouter_styles() {
+        wp_enqueue_style(
+            'style-principal'/* $handle - Identificateur du lien css*/,
+            get_template_directory_uri() . '/style.css'/* $src - Chemin ou se trouve le fichier style.css*/,
+            array() /* $deps - Les fichiers CSS qui dependent de style.css*/,
+            filemtime(get_template_directory() . '/style.css') /* $ver - Version de notre fichier style.css*/,
+        );
+    }
+
+    // add_action() - Adds a callback function to an action hook.
+    add_action( 'wp_enqueue_scripts', 'ajouter_styles' );
+
+    // Enregistrement des menus
+    if ( ! function_exists( 'enregistrement_nav_menu' ) ) {
+        function enregistrement_nav_menu(){
+            register_nav_menus( array(
+                'principal' => 'Menu principal',
+                'footer'  => 'Menu pied de page'
+            ) );
+        }
+        
+        // add_action() - Adds a callback function to an action hook.
+        add_action( 'after_setup_theme', 'enregistrement_nav_menu', 0 );
+    }
 ?>
--- header.php
+++ header.php
@@ -10,6 +10,11 @@
 
 <body>
     <header>
+        <!-- wp_nav_menu() - Displays a navigation menu. -->
+        <?php wp_nav_menu(array(
+            "menu" => "entete"
+        )) ?>
+
         <!-- bloginfo() - Permet d'afficher des informations sur votre site WordPress, telles que le titre, la description, l'url, etc. -->
         <!-- Va chercher le Titre du site et le Slogan du site. Lorsqu'on clique sur le nom du site, on peut revenir à la page d'accueil -->
         <h1> <a href="<?= bloginfo('url') ?>"><?= bloginfo('name') ?></a> </h1> - <h2><?= bloginfo('description') ?></h2>
