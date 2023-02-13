4ea66da56f341a69b80c90687f3617e8f3c9e856
Christophe Leam-Touch <e1449109@cmaisonneuve.qc.ca>
2023-02-13 13:28:57 -0500

    s4c1 - Création/Modification markdown, blocflex, root et front-page.php

--- front-page.php
+++ front-page.php
@@ -8,25 +8,27 @@
 <?php get_header() ?>
 
 <main>
-    <h3>La page d'accueil (Front Page)</h3>
-<?php
-    // have_posts() - Determines whether current WordPress query has posts to loop over. 
-    if (have_posts()):
-        // the_post() - Iterate the post index in the loop.
-        while (have_posts()) : the_post(); ?>
-            <!-- get_permalink() - Retrieves the full permalink for the current post or post ID. Permet d'aller chercher l'adresse du POST -->
-            <!-- get_the_title() - Retrieves the post title.  -->
-            <h1>
-                <a href="<?= get_permalink(); ?>"><?= get_the_title(); ?></a>
-            </h1>;
+    <section class="blocflex">
+        <?php
+            // have_posts() - Determines whether current WordPress query has posts to loop over. 
+            if (have_posts()):
+                // the_post() - Iterate the post index in the loop.
+                while (have_posts()) : the_post(); ?>
+                    <!-- get_permalink() - Retrieves the full permalink for the current post or post ID. Permet d'aller chercher l'adresse du POST -->
+                    <!-- get_the_title() - Retrieves the post title.  -->
+                    <article>
+                        <h1>
+                            <a href="<?= get_permalink(); ?>"><?= get_the_title(); ?></a>
+                        </h1>
 
-            <!-- wp_trim_words( $text, $num_words, $more, $original_text ) - Cette fonction permet de spécifier le nombre de caractère maximum à afficher en résumé -->
-            <!-- get_the_excerpt() - Cette fonction permet de récupérer un extrait (résumé) d'un article sans l'afficher. -->
-            <?= wp_trim_words(get_the_excerpt(), 10, "&#127829;"); ?>
-        <hr>
-        <?php endwhile;
-    endif;
-?>
+                        <!-- wp_trim_words( $text, $num_words, $more, $original_text ) - Cette fonction permet de spécifier le nombre de caractère maximum à afficher en résumé -->
+                        <!-- get_the_excerpt() - Cette fonction permet de récupérer un extrait (résumé) d'un article sans l'afficher. -->
+                        <p><?= wp_trim_words(get_the_excerpt(), 10, "&#127829;"); ?></p>
+                    </article>
+                <?php endwhile;
+            endif;
+        ?>
+    </section>
 </main>
 
 <!-- Permet d'aller chercher le fichier footer.php -->
