73868f0b44e2f14757d7dbfab769147f613b09d7
Christophe Leam-Touch <e1449109@cmaisonneuve.qc.ca>
2023-02-24 08:41:56 -0500

    S5C2 - Intégration de la classe WP_Query() dans category.php

--- category.php
+++ category.php
@@ -10,10 +10,18 @@
 <main>
     <section class="blocflex">
         <?php
+            // Intégration de la classe WP_Query() - La classe WP_Query() permet de récupérer des publications (articles, pages, types de publications personnalisés, etc.) en fonction de divers critères, tels que les catégories, les balises, les dates, les auteurs, etc.
+            $category = get_queried_object();
+            $args = array(
+                'category_name' => $category -> slug,
+                'orderby' => 'title',
+                'order' => 'ASC'
+            );
+            $query = new WP_Query($args);
             // have_posts() - Determines whether current WordPress query has posts to loop over. 
-            if (have_posts()):
+            if ($query -> have_posts()):
                 // the_post() - Iterate the post index in the loop.
-                while (have_posts()) : the_post(); ?>
+                while ($query -> have_posts()) : $query -> the_post(); ?>
                     <!-- get_permalink() - Retrieves the full permalink for the current post or post ID. Permet d'aller chercher l'adresse du POST -->
                     <!-- get_the_title() - Retrieves the post title.  -->
                     <article>
@@ -25,8 +33,9 @@
                         <!-- get_the_excerpt() - Cette fonction permet de récupérer un extrait (résumé) d'un article sans l'afficher. -->
                         <p><?= wp_trim_words(get_the_excerpt(), 10, "&#127829;"); ?></p>
                     </article>
-                <?php endwhile;
-            endif;
+                <?php endwhile; ?>
+            <?php endif;
+            wp_reset_postdata(); ?>
         ?>
     </section>
 </main>
